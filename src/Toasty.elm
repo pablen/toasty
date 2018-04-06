7870439e3f8f0b659cb596ddeb6b88b3e9ccee33
Matthias Devlamynck <matthias.devlamynck@mailoo.org>
2018-04-07 00:20:23 +0200

    Add support for persisting toats

--- src/Toasty.elm
+++ src/Toasty.elm
@@ -1,7 +1,7 @@
 module Toasty exposing
     ( Stack, Msg
     , config, delay, transitionOutDuration, containerAttrs, itemAttrs, transitionInAttrs, transitionOutAttrs, Config
-    , view, update, addToast, addToastIf, addToastIfUnique, hasToast, initialState
+    , view, update, addToast, addPersistentToast, addToastIf, addToastIfUnique, hasToast, initialState
     )
 
 {-| This package lets you easily show customizable toast notifications in your
@@ -122,7 +122,7 @@ classes.
 
 # Other functions
 
-@docs view, update, addToast, addToastIf, addToastIfUnique, hasToast, initialState
+@docs view, update, addToast, addPersistentToast, addToastIf, addToastIfUnique, hasToast, initialState
 
 -}
 
@@ -151,6 +151,17 @@ to be as complex or simple as you want.
 -}
 type Stack a
     = Stack (List ( Id, Status, a )) Seed
+
+
+{-| How the toast will be removed.
+
+Temporary toasts are removed after a timeout or after a click,
+Persistent toasts must be clicked to be removed.
+
+-}
+type RemoveBehaviour
+    = Temporary
+    | Persistent
 
 
 {-| The internal message type used by the library. You need to tag and add it to your app messages.
@@ -316,17 +327,27 @@ update function branches.
 
 -}
 addToast : Config msg -> (Msg a -> msg) -> a -> ( { m | toasties : Stack a }, Cmd msg ) -> ( { m | toasties : Stack a }, Cmd msg )
-addToast (Config cfg) tagger toast ( model, cmd ) =
-    let
-        (Stack toasts seed) =
-            model.toasties
+addToast =
+    addToast_ Temporary
 
-        ( newId, newSeed ) =
-            getNewId seed
-    in
-    ( { model | toasties = Stack (toasts ++ [ ( newId, Entered, toast ) ]) newSeed }
-    , Cmd.batch [ cmd, Task.perform (\() -> tagger (TransitionOut newId)) (Process.sleep <| cfg.delay) ]
-    )
+
+{-| Adds a toast that won't be removed after a timeout to the stack. It receives and returns
+a tuple of type '(model, Cmd msg)' so that you can easily pipe it to your app
+update function branches.
+
+    update msg model =
+        case msg of
+            SomeAppMsg ->
+                ( newModel, Cmd.none )
+                    |> Toasty.addPersistentToast myConfig ToastyMsg (MyToast "Entity successfully created!")
+
+            ToastyMsg subMsg ->
+                Toasty.update myConfig ToastyMsg subMsg model
+
+-}
+addPersistentToast : Config msg -> (Msg a -> msg) -> a -> ( { m | toasties : Stack a }, Cmd msg ) -> ( { m | toasties : Stack a }, Cmd msg )
+addPersistentToast =
+    addToast_ Persistent
 
 
 {-| Similar to `addToast` but also receives a condition parameter `List toast -> Bool`
@@ -368,6 +389,28 @@ hasToast toast (Stack toasts _) =
     toasts
         |> List.map (\( _, _, t ) -> t)
         |> List.member toast
+
+
+addToast_ : RemoveBehaviour -> Config msg -> (Msg a -> msg) -> a -> ( { m | toasties : Stack a }, Cmd msg ) -> ( { m | toasties : Stack a }, Cmd msg )
+addToast_ removeBehaviour (Config cfg) tagger toast ( model, cmd ) =
+    let
+        (Stack toasts seed) =
+            model.toasties
+
+        ( newId, newSeed ) =
+            getNewId seed
+
+        task =
+            case removeBehaviour of
+                Temporary ->
+                    Task.perform (\() -> tagger (TransitionOut newId)) (Process.sleep <| cfg.delay)
+
+                Persistent ->
+                    Cmd.none
+    in
+    ( { model | toasties = Stack (toasts ++ [ ( newId, Entered, toast ) ]) newSeed }
+    , Cmd.batch [ cmd, task ]
+    )
 
 
 {-| Renders the stack of toasts. You need to add it to your app view function and
